0f3eb4f9aebf5d6ee458260705d494d2624a15e8
v-pun215 <vihaan.pundir26@gmail.com>
2025-12-22 21:22:07 +0530

    abc

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-print(0%2)
+li = [1,2,3,4]
+google = li-[1,2]
+
+print(google)
---
+++ usaco/abc.py
@@ -0,0 +1,9 @@
+nums = list(map(int, input().split()))
+nums.sort()
+
+
+a = nums[0]
+b= nums[1]
+c = nums[-1]-a-b
+
+print(a,b,c)

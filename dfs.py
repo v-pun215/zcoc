9effd4e7aa9db6ef507e9dde6ec5c2440b634fef
Vihaan <vihaan.pundir26@gmail.com>
2026-01-21 13:05:52 +0000

    dfs! bfs is better honestly lol

--- dfs.py
+++ dfs.py
@@ -9,6 +9,7 @@ r'''
          \ /
           6
 '''
+from collections import deque
 v = [1,2,3,4,5,6,7,8]
 ad = {
     1: [2,3],
@@ -20,3 +21,32 @@ ad = {
     7: [6, 8],
     8: [4, 7]
 }
+
+
+def DFS(graph, node, marked, parent):
+    print(node)
+    marked.add(node)
+    for neighbour in graph[node]:
+
+        if not neighbour in marked:
+            parent[neighbour] = node
+            print("arrived at", neighbour)
+            DFS(graph, neighbour, marked, parent)
+
+marked = set()
+parent = {}
+DFS(ad, 1, marked, parent)
+print(parent)
+def retrace_steps(start,  finish, parent):
+    li = deque()
+    li.append(finish)
+    while True:
+
+        first =  li[0]
+        parente = parent[first]
+        li.appendleft(parente)
+        if parente == 1:
+            break
+    return list(li)
+
+print(retrace_steps(1, 8, parent))

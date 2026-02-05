18d0d4285932535eb01e11386307ac216d38a9a7
v-pun215 <vihaan.pundir26@gmail.com>
2026-02-05 12:13:00 +0530

    great

--- zco2020/weird_ordering.py
+++ zco2020/weird_ordering.py
@@ -1,4 +1,6 @@
 #input management
+import sys
+#all_inp = sys.stdin.read().splitlines()
 T = int(input())
 
 def power(x,y):
@@ -21,13 +23,41 @@ def order(arr, i):
     r = order(r, i+1)
     c = concatenate(l,r)
     return c
+
 inputs = []
-for _ in range(T):
-    inputs.append(list(map(int, input().split())))
+for i in range(T):
+    ben_stocks = list(map(int, input().split()))
+    inputs.append(ben_stocks)
+'''
+import time
+start = time.time()
 for i in inputs:
+
     p, idx = i
     a = []
     for i in range(2**p):
         a.append(i)
     ans = order(a, 0)
     print(ans[idx])
+end = time.time() - start
+print("time taken:",end)
+
+'''
+def solve(p, idx):
+    result = 0
+    current_idx = idx
+    part_size = 2**(p-1)
+
+
+    for i in range(p):
+        if current_idx>=part_size:
+            result+=2**i
+            current_idx-=part_size
+        part_size/=2
+
+    return result
+
+
+for inp in inputs:
+    p, idx = inp
+    print(solve(p,idx))
